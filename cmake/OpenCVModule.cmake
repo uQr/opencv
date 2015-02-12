fe07676822ae03f3707eb13ef6e249855721f4e0
Evgeny Agafonchikov <evgeny.agafonchikov@akvelon.com>
2015-02-12 15:11:58 +0300

    Add /ZW flag for tests on WINRT

--- cmake/OpenCVModule.cmake
+++ cmake/OpenCVModule.cmake
@@ -950,6 +950,8 @@ function(ocv_add_perf_tests)
         add_custom_command(TARGET "opencv_perf_${name}"
                            POST_BUILD
                            COMMAND link.exe /edit /APPCONTAINER:NO $(TargetPath))
+
+        target_compile_options("opencv_perf_${name}" PUBLIC "/ZW")
       endif()
 
       if(NOT BUILD_opencv_world)
@@ -1016,6 +1018,7 @@ function(ocv_add_accuracy_tests)
                            POST_BUILD
                            COMMAND link.exe /edit /APPCONTAINER:NO $(TargetPath))
 
+        target_compile_options("opencv_test_${name}" PUBLIC "/ZW")
       endif()
       if(NOT BUILD_opencv_world)
         _ocv_add_precompiled_headers(${the_target})
