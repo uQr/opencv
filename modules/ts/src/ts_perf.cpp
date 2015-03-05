19fae5b188f5e2b4bd88bda717462850fbede3cb
Evgeny Agafonchikov <evgeny.agafonchikov@akvelon.com>
2015-03-05 16:24:41 +0300

    Read/write perf results on WINRT

--- modules/ts/src/ts_perf.cpp
+++ modules/ts/src/ts_perf.cpp
@@ -669,11 +669,13 @@ static int perf_validation_idle_delay_ms = 3000; // 3 sec
 static void loadPerfValidationResults(const std::string& fileName)
 {
     perf_validation_results.clear();
+
+    float value = 0;
+#ifndef WINRT
     std::ifstream infile(fileName.c_str());
     while (!infile.eof())
     {
         std::string name;
-        float value = 0;
         if (!(infile >> value))
         {
             if (infile.eof())
@@ -689,8 +691,29 @@ static void loadPerfValidationResults(const std::string& fileName)
         }
         if (!name.empty() && name[name.size() - 1] == '\r') // CRLF processing on Linux
             name.resize(name.size() - 1);
+#else
+    char name[256];
+    FILE* infile = fopen(fileName.c_str(), "r");
+
+    if (!infile)
+    {
+        std::cout << "ERROR: Can't load performance validation results from " << fileName << "!" << std::endl;
+        return;
+    }
+
+    while (!feof(infile))
+    {
+        if (!fscanf(infile, "%f;%s\n", &value, name))
+        {
+            std::cout << "ERROR: Can't load performance validation results from " << fileName << "!" << std::endl;
+            return;
+        }
+#endif
         perf_validation_results[name] = value;
     }
+#ifdef WINRT
+    fclose(infile);
+#endif
     std::cout << "Performance validation results loaded from " << fileName << " (" << perf_validation_results.size() << " entries)" << std::endl;
 }
 
@@ -703,14 +726,29 @@ static void savePerfValidationResults()
 {
     if (!perf_validation_results_outfile.empty())
     {
-        std::ofstream outfile((perf_validation_results_directory + perf_validation_results_outfile).c_str());
         std::map<std::string, float>::const_iterator i;
+#ifndef WINRT
+        std::ofstream outfile((perf_validation_results_directory + perf_validation_results_outfile).c_str());
+#else
+        FILE* outfile = fopen((perf_validation_results_directory + perf_validation_results_outfile).c_str(), "w+");
+        if (outfile)
+        {
+#endif
         for (i = perf_validation_results.begin(); i != perf_validation_results.end(); ++i)
         {
+#ifndef WINRT
             outfile << i->second << ';';
             outfile << i->first << std::endl;
+#else
+            fprintf(outfile, "%f;%s\n", i->second, (i->first).c_str());
+#endif
         }
+#ifndef WINRT
         outfile.close();
+#else
+        fclose(outfile);
+        }
+#endif
         std::cout << "Performance validation results saved (" << perf_validation_results.size() << " entries)" << std::endl;
     }
 }
@@ -890,11 +928,10 @@ void TestBase::Init(const std::vector<std::string> & availableImpls,
 #endif
 
     {
-//TODO: replace temporary solution
 #ifndef WINRT
         const char* path = getenv("OPENCV_PERF_VALIDATION_DIR");
 #else
-        const char* path = "/";
+        const char* path = OPENCV_PERF_VALIDATION_DIR;
 #endif
         if (path)
             perf_validation_results_directory = path;
