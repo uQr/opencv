4751e8a21de39cbdf030d50f1fa54b344c8afaf8
Maxim Kostin <maxim.kostin@akvelon.com>
2015-04-08 13:17:02 +0300

    Restored winrt_universal/VideoCapture sample app, sources and headers required by VideoCapture sample. Removed obsolete project references from VideoCapture sample.

---
+++ samples/winrt_universal/VideoCapture/VideoCapture/VideoCapture.WindowsPhone/MainPage.xaml.cpp
@@ -0,0 +1,45 @@
+﻿//
+// MainPage.xaml.cpp
+// Implementation of the MainPage class.
+//
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+
+using namespace VideoCapture;
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+using namespace Windows::Graphics::Display;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+MainPage::MainPage()
+{
+	InitializeComponent();
+    Windows::UI::ViewManagement::StatusBar::GetForCurrentView()->HideAsync();
+    Application::Current->DebugSettings->EnableFrameRateCounter = false;
+    DisplayInformation::AutoRotationPreferences = DisplayOrientations::Landscape;
+}
+
+/// <summary>
+/// Invoked when this page is about to be displayed in a Frame.
+/// </summary>
+/// <param name="e">Event data that describes how this page was reached.  The Parameter
+/// property is typically used to configure the page.</param>
+void MainPage::OnNavigatedTo(NavigationEventArgs^ e)
+{
+	(void) e;	// Unused parameter
+
+
+    m_main = std::unique_ptr<AppMain>(new AppMain(Preview));
+    m_main->start();
+}
